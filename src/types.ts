0e2cf79db88b799b9b79aaa47e64193cae1a2ad3
WakelessSloth56 <wsloth56@foxmail.com>
2023-08-23 00:46:19 +0800

    fix(web): add readonly markers
    Co-authored-by: lainio24 <lainio24@outlook.com>

--- src/types.ts
+++ src/types.ts
@@ -1,63 +1,78 @@
 export type StringKV = { [x: string]: string };
 
-type ItemIndex = { id: string; type: number; user: number; time: number };
+type ItemIndex = Readonly<{
+    id: string;
+    type: number;
+    user: number;
+    time: number;
+}>;
 
-type ItemTagIndex = { id: number; type: number; name: string; count: number };
+type ItemTagIndex = Readonly<{
+    id: number;
+    type: number;
+    name: string;
+    count: number;
+}>;
 
-type UserIndex = { id: number; name: string; sex: number; count: number };
+type UserIndex = Readonly<{
+    id: number;
+    name: string;
+    sex: number;
+    count: number;
+}>;
 
-type UserTagIndex = { id: number; name: string; count: number };
+type UserTagIndex = Readonly<{ id: number; name: string; count: number }>;
 
-export type IndexData = {
+export type IndexData = Readonly<{
     item: ItemIndex;
     itemtag: ItemTagIndex;
     user: UserIndex;
     usertag: UserTagIndex;
-};
+}>;
 
-type ItemDetail = {
+type ItemDetail = Readonly<{
     id: string;
     type: number;
     time: number;
     user: number;
     content: string;
     tags: number[];
-    images: {
+    images: Readonly<{
         id: number;
         name: string;
         format: number;
         w: number;
         h: number;
         size: number;
-    }[];
-};
+    }>[];
+}>;
 
-type ItemTagDetail = {
+type ItemTagDetail = Readonly<{
     id: number;
     type: number;
     name: string;
     items: string[];
-};
+}>;
 
-type UserDetail = {
+type UserDetail = Readonly<{
     id: number;
     sex: number;
     names: string[];
     tags: number[];
     items: string[];
-};
+}>;
 
-type UserTagDetail = {
+type UserTagDetail = Readonly<{
     id: number;
     name: string;
     users: number[];
-};
+}>;
 
-export type DetailData = {
+export type DetailData = Readonly<{
     item: ItemDetail;
     itemtag: ItemTagDetail;
     user: UserDetail;
     usertag: UserTagDetail;
-};
+}>;
 
 export type TYPE = keyof IndexData & keyof DetailData;
